f5f4374cd042c4963cacb5fbf5441547fc8848ce
lakiert <aleksandra.lakiert@gmail.com>
2021-04-01 19:45:27 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+# zad1
+# printf('zad 1')
+# a = np.arange(4,4*20+1,4)
+# print(a)
+
+
+# zad2
+# printf('zad 2')
+# a = np.array([0.5, 2.3, 5.7, 6.1])
+# b = a.astype('int32')
+#
+# print(a)
+# print(a.dtype)
+# print(b)
+# print(b.dtype)
+
+
+#zad3
+# printf('zad 3')
+# def funkcja(n):
+#     tab = np.arange((n*n))
+#     tab = tab.reshape((n,n))
+#     for x in range(n, n):
+#         for y in range(n, n):
+#             tab[x, y] = 2**n
+#
+#     print(tab)
+# funkcja(5)
+
+
+#zad4
+# printf('zad 4')
+# def funkcja(a, ile):
+#     tab = np.logspace(1.0, ile, num=ile, dtype=int, base=a)
+#     print(tab)
+#
+# funkcja(2, 4)
+# funkcja(2, 6)
+# funkcja(3, 3)
+
+
+
+
+
+
+
+
+
+
+
+
